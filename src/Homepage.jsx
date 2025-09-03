2f7de26ac1de3ce79bd55b980fd43f85883ba750
moen <moenr49@gmail.com>
2025-09-03 13:13:45 +0300

    craeting homepage and importing it to main

---
+++ src/Homepage.jsx
@@ -0,0 +1,19 @@
+function Homepage() {
+  return (
+    <>
+      <div className="logo">
+        <h1>Ranges Millers</h1>
+        <nav  className="nav-menu">
+        <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Homepage from './Homepage'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <Homepage />
   </StrictMode>,
 )
